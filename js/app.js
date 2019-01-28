2b224f868c76c610f13fd035671af764a1402f5b
lucamotta <l.motta@reply.it>
2019-01-28 16:26:33 +0100

    Add sky

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var container, stats;
-var camera, scene, renderer;
+var camera, scene, renderer, sky, sunSphere;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
@@ -21,6 +21,8 @@ function init() {
     loadModel();
 
     initRenderer();
+
+    initSky();
 
     initEventsListeners();
 }
@@ -98,6 +100,55 @@ function initTrackballControls() {
   controls.dynamicDampingFactor = 0.15;
 
   controls.keys = [ 65, 83, 68 ];
+}
+
+function initSky() {
+  // Add Sky
+  sky = new THREE.Sky();
+  sky.scale.setScalar( 450000 );
+  scene.add( sky );
+
+  // Add Sun Helper
+  sunSphere = new THREE.Mesh(
+    new THREE.SphereBufferGeometry( 20000, 16, 8 ),
+    new THREE.MeshBasicMaterial( { color: 0xffffff } )
+  );
+  sunSphere.position.y = - 700000;
+  sunSphere.visible = false;
+  scene.add( sunSphere );
+
+  /// GUI
+
+  var effectController  = {
+    turbidity: 10,
+    rayleigh: 2,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.8,
+    luminance: 1,
+    inclination: 0.49, // elevation / inclination
+    azimuth: 0.25, // Facing front,
+    sun: ! true
+  };
+
+  var distance = 400000;
+  var uniforms = sky.material.uniforms;
+  uniforms.turbidity.value = effectController.turbidity;
+  uniforms.rayleigh.value = effectController.rayleigh;
+  uniforms.luminance.value = effectController.luminance;
+  uniforms.mieCoefficient.value = effectController.mieCoefficient;
+  uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
+
+  var theta = Math.PI * ( effectController.inclination - 0.5 );
+  var phi = 2 * Math.PI * ( effectController.azimuth - 0.5 );
+
+  sunSphere.position.x = distance * Math.cos( phi );
+  sunSphere.position.y = distance * Math.sin( phi ) * Math.sin( theta );
+  sunSphere.position.z = distance * Math.sin( phi ) * Math.cos( theta );
+  sunSphere.visible = effectController.sun;
+
+  uniforms.sunPosition.value.copy( sunSphere.position );
+
+  renderer.render( scene, camera );
 }
 
 function loadModel() {
